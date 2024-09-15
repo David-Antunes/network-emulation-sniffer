80931b27d8ccc56e2aabecefede16c0097a80a48
David Antunes <dmm.antunes@campus.fct.unl.pt>
2024-09-15 21:28:52 +0100

    Print packet information to stdout

--- go.mod
+++ go.mod
@@ -3,12 +3,17 @@ module github.com/David-Antunes/network-emulation-sniffer
 go 1.23.1
 
 require (
-	github.com/David-Antunes/network-emulation-proxy v0.0.0-20240913142237-30243d9589c3 // indirect
+	github.com/David-Antunes/network-emulation-proxy v0.0.0-20240913142237-30243d9589c3
+	github.com/google/gopacket v1.1.19
+)
+
+require (
 	github.com/cilium/ebpf v0.16.0 // indirect
 	github.com/david-antunes/xdp v1.0.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/vishvananda/netlink v1.3.0 // indirect
 	github.com/vishvananda/netns v0.0.4 // indirect
 	golang.org/x/exp v0.0.0-20240909161429-701f63a606c0 // indirect
+	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,28 +3,41 @@ package main
 import (
 	"encoding/gob"
 	"fmt"
+	"log"
 	"net"
 	"os"
 
 	"github.com/David-Antunes/network-emulation-proxy/xdp"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcapgo"
 )
 
 func main() {
 
 	if len(os.Args) == 1 {
 		fmt.Println("missing socket id")
-		return 
+		return
 	}
 	if len(os.Args) > 2 {
 		fmt.Println("too many arguments")
-		return 
+		return
 	}
-	conn, err := net.Dial("unix", "/tmp/"+os.Args[1]+".sniff")
+	conn, err := net.Dial("unix", os.Args[1])
 
 	if err != nil {
 		panic(err)
 	}
 	dec := gob.NewDecoder(conn)
+        f, err := os.Create("temp.pcap")
+        if err != nil {
+                log.Fatal(err)
+                os.Exit(1)
+        }
+        defer f.Close()
+
+        w := pcapgo.NewWriter(os.Stdout)
+        w.WriteFileHeader(uint32(65535), layers.LinkTypeEthernet)
 	for {
 		var frame *xdp.Frame
 
@@ -32,7 +45,8 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(frame.FramePointer)
+		packet := gopacket.NewPacket(frame.FramePointer, layers.LinkTypeEthernet, gopacket.Default)
+        w.WritePacket(gopacket.CaptureInfo{Timestamp: frame.Time, CaptureLength: frame.FrameSize, Length: frame.FrameSize,InterfaceIndex: 1}, packet.Data())
 	}
 
 }
